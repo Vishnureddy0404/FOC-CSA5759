5e6dc4f697369057990dc67950f27087c211dcc2
vishnureddy1729 <124245030+vishnureddy1729@users.noreply.github.com>
2023-02-06 14:28:56 +0530

    Create square and cube.c

---
+++ square and cube.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main() {
+
+    float n;
+    printf("Enter a decimal number :");
+    scanf("%f",&n);   
+
+    printf("\nSquare of the decimal number %f is %f",n,n*n);
+
+    printf("\nCube of the decimal number %f is %f",n,n*n*n);
+
+}
