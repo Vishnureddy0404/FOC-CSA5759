6bbd19c9b55a956eff584cce6726e50e86af80fc
Vishnuvardhanreddy bandi <124245030+vishnureddy1729@users.noreply.github.com>
2023-08-28 22:00:19 +0530

    Update Decimal to binary.c

--- Decimal to binary.c
+++ Decimal to binary.c
@@ -5,9 +5,9 @@ int main()
   printf("Enter an integer in decimal number system\n:");
   scanf("%d", &n);
   printf("%d in binary number system is:\n", n);
-  for (i = 31; i >= 0; i--)
+  for (i=31; i>= 0; i--)
   {
-    k = n >> i;
+    k = n>> i;
     if (k & 1)
       printf("1");
     else
